bf659cd92e052d2598413fb3d193bfd8e7df1f10
Fang <fang@fang.io>
2018-03-29 01:14:21 +0200

    Touch up Pool contract and fix tests.

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -1,5 +1,4 @@
-// an urbit star pool
-// draft
+// an example urbit star pool
 
 pragma solidity 0.4.18;
 
@@ -11,32 +10,47 @@ import './Constitution.sol';
 
 contract Pool is MintableToken, BurnableToken
 {
-  // token details
+  //  ERC20 token metadata
+  //
   string constant public name = "StarToken";
-  string constant public symbol = "STA";
+  string constant public symbol = "TAR";
   uint constant public decimals = 18;
   uint256 constant public oneStar = 1e18;
 
-  // store reference to the ship contract, because it is constant.
-  // the current constitution is always ships.owner.
+  //  ships: ships state data store
+  //
   Ships public ships;
 
-  // keep track of assets for public viewing.
+  //  assets: stars currently held in this pool
+  //
   uint16[] public assets;
-  // per asset: index in assets array (for efficient deletion).
-  //NOTE these describe the "nth array element", so they're at index n-1.
-  mapping(uint16 => uint256) public assetNumbers;
 
+  //  assetIndexes: per star, (index + 1) in :assets
+  //
+  //    We delete assets by moving the last entry in the array to the
+  //    newly emptied slot, which is (n - 1) where n is the value of
+  //    assetIndexes[star].
+  //
+  mapping(uint16 => uint256) public assetIndexes;
+
+  //  Pool(): register ships state data store
+  //
   function Pool(Ships _ships)
     public
   {
     ships = _ships;
-    // needs to be its own owner so that it can mint.
-    // this pool design is completely hands-off after launching, so the contract
-    // creator doesn't need special permissions anyway.
+
+    //  make this contract its own owner, to allow it to mint and burn tokens,
+    //  and prevent the original creator from doing so themselves
+    //
     owner = this;
   }
 
+  //  getAllAssets(): return array of assets held by this contract
+  //
+  //    Note: only useful for clients, as Solidity does not currently
+  //    support returning dynamic arrays.
+  //
   function getAllAssets()
     view
     external
@@ -51,67 +65,142 @@ contract Pool is MintableToken, BurnableToken
   //    pool configured as the transferrer for that star.
   // 2. the star must be latent, the sender the owner of its parent galaxy, and
   //    this pool configured as a launcher for that galaxy.
+
+  //  deposit(): add a star _star to the pool, receive a token in return
+  //
+  //    to be able to deposit, either of the following conditions must be
+  //    satisfied:
+  //
+  //    (1) the caller is the owner of the star, the star has a key revision
+  //    of zero, and this contract is configured as the star's transfer proxy
+  //
+  //    (2) the caller is the owner of the parent of the star, the star is
+  //    inactive, and this contract is configured at the galaxy's spawn proxy
+  //
+  //    Note: in the if-else-if blocks below, any checks beyond the first
+  //    practically don't matter. it would be fine to let the constitution
+  //    revert in our stead, but we implement the checks anyway to make the
+  //    required conditions clear through local contract code.
+  //
   function deposit(uint16 _star)
     external
     isStar(_star)
   {
-    // there are two possible ways to deposit a star:
-    // 1: for locked stars, grant the pool permission to transfer ownership of
-    //    that star. the pool will transfer the deposited star to itself.
-    if (ships.isPilot(_star, msg.sender)
-             && ships.isTransferrer(_star, this))
+    //  case (1)
+    //
+    if ( //  :msg.sender must be the _star's owner
+         //
+         ships.isOwner(_star, msg.sender) &&
+         //
+         //  the _star may not have been used yet
+         //
+         ( 0 == ships.getKeyRevisionNumber(_star) ) &&
+         //
+         //  :this contract must be allowed to transfer the _star
+         //
+         ships.isTransferProxy(_star, this) )
     {
-      // only accept stars that aren't alive, that are reputationless, "clean".
-      require(!ships.isState(_star, Ships.State.Living));
-      // attempt to transfer the star to us.
+      //  transfer ownership of the _star to :this contract
+      //
       Constitution(ships.owner()).transferShip(_star, this, true);
     }
-    // 2: for latent stars, grant the pool launch permission on a galaxy.
-    //    the pool will launch the deposited star directly to itself.
-    else if (ships.isPilot(ships.getOriginalParent(_star), msg.sender)
-        && ships.isLauncher(ships.getOriginalParent(_star), this))
+    //
+    //  case (2)
+    //
+    else if ( //  :msg.sender must be the _star's prefix's owner
+              //
+              ships.isOwner(ships.getPrefix(_star), msg.sender) &&
+              //
+              //  the _star must be inactive
+              //
+              !ships.isActive(_star) &&
+              //
+              //  :this contract must be allowed to spawn the _star
+              //
+              ships.isSpawnProxy(ships.getPrefix(_star), this) )
     {
-      // attempt to launch the star to us.
-      Constitution(ships.owner()).launch(_star, this, 0);
+      //  transfer ownership of the _star to :this contract
+      //
+      Constitution(ships.owner()).spawn(_star, this);
     }
-    // if neither of those are possible, error out.
+    //
+    //  if neither case is applicable, abort the transaction
+    //
     else
     {
       revert();
     }
+
+    //  update state to include the deposited star
+    //
     assets.push(_star);
-    assetNumbers[_star] = assets.length;
-    // we succeeded, so grant the sender their token.
-    // we use this.call here so that msg.sender gets set to us.
-    // the operations won't run correctly otherwise.
+    assetIndexes[_star] = assets.length;
+
+    //  mint a star token and grant it to the :msg.sender
+    //
+    //    Note: this.call is used so that the :msg.sender in its context
+    //    gets set to this contract's address. this is necessary to
+    //    satisfy the permission check in the mint function.
+    //
     this.call.gas(50000)(bytes4(keccak256("mint(address,uint256)")),
                          msg.sender, oneStar);
   }
 
-  // take one star from the pool.
+  //  withdraw(): pay a token, receive the most recently deposited star
+  //
+  //TODO  why does this overshadow withdraw(uint16)?
+  /* function withdraw()
+    public
+  {
+    withdraw(assets[assets.length-1]);
+  } */
+
+  //  withdraw(): pay a token, receive the star _star in return
+  //
   function withdraw(uint16 _star)
-    external
+    public
     isStar(_star)
   {
-    // retrieve current asset index.
-    uint256 i = assetNumbers[_star];
+    //  make sure the :msg.sender has sufficient balance
+    //
+    require(balanceOf(msg.sender) >= oneStar);
+
+    //  do some gymnastics to keep the list of owned assets gapless.
+    //  delete the withdrawn asset from the list, then fill that gap with
+    //  the list tail
+
+    //  i: current index of _star in list of assets
+    //
+    uint256 i = assetIndexes[_star];
+
+    //  we store index + 1, because 0 is the eth default value
+    //
     require(i > 0);
     i--;
-    // copy last asset to current index.
+
+    //  copy the last item in the list into the now-unused slot
+    //
     uint256 last = assets.length - 1;
     uint16 move = assets[last];
     assets[i] = move;
-    // delete last asset.
+
+    //  delete the last item
+    //
     delete(assets[last]);
     assets.length = last;
-    assetNumbers[_star] = 0;
-    // we own one less star, so burn one token.
+    assetIndexes[_star] = 0;
+
+    //  we own one less star, so burn one token.
+    //
     burn(oneStar);
-    // attempt to transfer the sender their star.
+
+    //  transfer ownership of the _star to :msg.sender
+    //
     Constitution(ships.owner()).transferShip(_star, msg.sender, true);
   }
 
-  // test if the ship is, in fact, a star.
+  //  test if _star is a star, not a galaxy
+  //
   modifier isStar(uint16 _star)
   {
     require(_star > 255);
--- test/TestPool.js
+++ test/TestPool.js
@@ -1,10 +1,10 @@
 const Ships = artifacts.require('../contracts/Ships.sol');
-const Votes = artifacts.require('../contracts/Votes.sol');
+const Polls = artifacts.require('../contracts/Polls.sol');
 const Constitution = artifacts.require('../contracts/Constitution.sol');
 const Pool = artifacts.require('../contracts/Pool.sol');
 
 contract('Pool', function([owner, user1, user2]) {
-  let ships, votes, constit, pool;
+  let ships, polls, constit, pool;
   const LATENT = 0;
   const LOCKED = 1;
   const LIVING = 2;
@@ -15,13 +15,13 @@ contract('Pool', function([owner, user1, user2]) {
 
   before('setting up for tests', async function() {
     ships = await Ships.new();
-    votes = await Votes.new();
-    constit = await Constitution.new(ships.address, votes.address);
+    polls = await Polls.new();
+    constit = await Constitution.new(0, ships.address, polls.address);
     await ships.transferOwnership(constit.address);
-    await votes.transferOwnership(constit.address);
-    await constit.createGalaxy(0, user1, 0, 0);
-    await constit.start(0, 10, {from:user1});
-    await constit.launch(512, user2, 0, {from:user1});
+    await polls.transferOwnership(constit.address);
+    await constit.createGalaxy(0, user1);
+    await constit.configureKeys(0, 10, 11, {from:user1});
+    await constit.spawn(512, user2, {from:user1});
     pool = await Pool.new(ships.address);
   });
 
@@ -40,7 +40,7 @@ contract('Pool', function([owner, user1, user2]) {
     } catch(err) {
       assertJump(err);
     }
-    await constit.grantLaunchRights(0, pool.address, {from:user1});
+    await constit.setSpawnProxy(0, pool.address, {from:user1});
     // must fail if caller is not galaxy owner.
     try {
       await pool.deposit(256);
@@ -50,7 +50,7 @@ contract('Pool', function([owner, user1, user2]) {
     }
     // deposit as galaxy owner.
     await pool.deposit(256, {from:user1});
-    assert.isTrue(await ships.isPilot(256, pool.address));
+    assert.isTrue(await ships.isOwner(256, pool.address));
     assert.equal(await pool.balanceOf(user1), 1000000000000000000);
     let res = await pool.getAllAssets();
     assert.equal(res.length, 1);
@@ -65,7 +65,7 @@ contract('Pool', function([owner, user1, user2]) {
     } catch(err) {
       assertJump(err);
     }
-    await constit.allowTransferBy(512, pool.address, {from:user2});
+    await constit.setTransferProxy(512, pool.address, {from:user2});
     // can't deposit if not owner.
     try {
       await pool.deposit(512, {from:user1});
@@ -75,7 +75,7 @@ contract('Pool', function([owner, user1, user2]) {
     }
     // deposit as star owner.
     await pool.deposit(512, {from:user2});
-    assert.isTrue(await ships.isPilot(512, pool.address));
+    assert.isTrue(await ships.isOwner(512, pool.address));
     assert.equal(await pool.balanceOf(user2), 1000000000000000000);
     let res = await pool.getAllAssets();
     assert.equal(res.length, 2);
@@ -91,20 +91,19 @@ contract('Pool', function([owner, user1, user2]) {
     } catch(err) {
       assertJump(err);
     }
-    // // withdraw a star
+    // withdraw a star
     await pool.withdraw(256, {from:user1});
-    assert.isTrue(await ships.isPilot(256, user1));
+    assert.isTrue(await ships.isOwner(256, user1));
     assert.equal((await pool.balanceOf(user1)), 0);
     let res = await pool.getAllAssets();
     assert.equal(res.length, 1);
     assert.equal(res[0].toNumber(), 512);
-    // // can't withdraw without balance.
+    // can't withdraw without balance.
     try {
       await pool.withdraw(512, {from:user1});
       assert.fail('should have thrown before');
     } catch(err) {
-      assert.isAbove(err.message.search('invalid opcode'), -1,
-        'Invalid opcode must be returned, but got ' + err);
+      assertJump(err);
     }
   });
 });
